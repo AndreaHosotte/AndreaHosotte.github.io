e8508f90a53cafb8517e5dde532a0968e797a875
Andrea Hosotte <91092446+AndreaHosotte@users.noreply.github.com>
2023-12-18 18:15:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener('scroll', function() {
 
 // TYPED
 var typed = new Typed('.typed', {
-    strings: ['Bonjour à tous, je me présente, je m’appelle Andrea Hosotte et j’ai', 'Bonjour, je m’appelle Andrea Hosotte, j’ai 18 ans et je suis étudiant en première année à l’école d’informatique Epitech. La majorité de ce que j’ai pu apprendre durant la première partie de mon apprentissage était en C mais j’ai quand même développé certaine connaissance dans différents langages comme le Javascript, le CSS ou encore l’HTML. Je suis quelqu’un de passionné par ce qu’il entreprend et qui n’hésite pas à se donner les moyens de ses ambitions. J’espère pouvoir poursuivre mon projet professionnel dans le domaine de l’informatique.'],
+    strings: ['Bonjour à tous, je me présente, je m’appelle Andrea Hosotte et j’ai', 'Bonjour, je m’appelle Andrea Hosotte, j’ai 20 ans et je suis étudiant en troisième année à l’école d’informatique Epitech. La majorité de ce que j’ai pu apprendre durant la première partie de mon apprentissage était en C mais j’ai quand même développé certaine connaissance dans différents langages comme le Javascript, le CSS ou encore l’HTML. Je suis quelqu’un de passionné par ce qu’il entreprend et qui n’hésite pas à se donner les moyens de ses ambitions. J’espère pouvoir poursuivre mon projet professionnel dans le domaine de l’informatique.'],
     typeSpeed: 20,
 });
 
